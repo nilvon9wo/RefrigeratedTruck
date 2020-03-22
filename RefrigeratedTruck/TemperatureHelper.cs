bf8480b4545821581d4fbb5fc20e6f14c803e9af
Brian Kessler <kessler.bm@gmail.com>
2020-03-22 22:13:20 +0100

    Add project files.

---
+++ RefrigeratedTruck/TemperatureHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace RefrigeratedTruck {
+    internal class GeometryHelper {
+        public double Degrees2Radians(double deg) {
+            return deg * Math.PI / 180;
+        }
+
+
+    }
+}
